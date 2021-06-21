56f8a8844bcb328a41e0f6faccda8355b26668f9
Farah A <farahwamer@gmail.com>
2021-06-21 12:26:08 -0400

    auto switching title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,46 @@
-import React from "react";
+import { React, useState } from "react";
 import "../styles/Home.css";
-import background from "../assets/home-bg.svg";
+import background from "../assets/home-bg.png";
 import Navbar from "./Navbar";
 
 const Home = () => {
+  const wordCycle = ["business", "service", "product", "idea", "dream"];
+  const [word, setWord] = useState({
+    index: 0,
+    word: wordCycle[0],
+  });
+  var intervalId = null;
+
+  const switchWord = () => {
+    setWord((prev) => {
+      if (prev.index === 4) {
+        setWord({ index: 0, word: wordCycle[0] });
+      } else {
+        setWord({ index: prev.index + 1, word: wordCycle[prev.index + 1] });
+      }
+    });
+  };
+
+  const startCycle = () => {
+    intervalId = setInterval(switchWord, 4000);
+  };
+
+  const stopCycle = () => {
+    clearInterval(intervalId);
+  };
+
   return (
     <div
       className="home-container"
       style={{ backgroundImage: `url(${background})` }}
       id="home"
+      onMouseEnter={startCycle}
     >
       <Navbar />
       <div className="home-content-container">
-        <h1 className="title">Take your business to the next level.</h1>
+        <h1 className="title">
+          Take your <mark>{word.word}</mark> <br></br>to the next level.
+        </h1>
         <a href="#contact">
           <button className="cta-btn">Get started</button>
         </a>
