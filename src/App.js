28ce91abdb54d458471b3f8f6e8792abe6107708
Farah A <farahwamer@gmail.com>
2021-06-26 18:21:42 -0400

    design change, restarting :)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import "./App.css";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import Services from "./components/Services";
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-      <Services />
-      <About />
-      <Contact />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
